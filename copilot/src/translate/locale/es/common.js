ac9084d5d64234c78f8e92c783e4d26da1c28a15
maria.rodrigues <maria.rodrigues@take.net>
2023-07-17 10:11:41 -0300

    copilot

---
+++ copilot/src/translate/locale/es/common.js
@@ -0,0 +1,36 @@
+const translations = {
+    title: {
+        homePage: 'Copilot complementos',
+        exemples: 'Ejemplos',
+        systemInfo: 'Información del sistema',
+        userInfo: 'Información del usuario'
+    },
+    paragraph: {
+        homeDescription: {
+            part1: 'Este es un complemento de plantilla para Copilot.',
+            part2: 'Edite <1>src/pages/Home.js</1> y guarde para recargar.'
+        }
+    },
+    link: {
+        blipDataExample: 'Obtener datos de blip',
+        swrExemple: 'Obtener datos con SWR'
+    },
+    button: {
+        repository: 'Visite el Repositorio'
+    },
+    menu: {
+        goBack: 'Volver'
+    },
+    name: 'Nombre',
+    shortName: 'Nombre corto',
+    email: 'E-mail',
+    phone: 'Teléfono',
+    identity: 'Identificador',
+    identifier: 'Identificador',
+    language: 'Idioma',
+    accessKey: 'Llave de acceso',
+    template: 'Plantilla',
+    creationDate: 'Fecha de creación'
+};
+
+export default translations;
---
+++ copilot/src/translate/locale/pt-br/common.js
@@ -0,0 +1,36 @@
+const translations = {
+    title: {
+        homePage: 'Copilot Plugin',
+        exemples: 'Exemplos',
+        systemInfo: 'Informações do sistema',
+        userInfo: 'Informações do usuário'
+    },
+    paragraph: {
+        homeDescription: {
+            part1: 'Este é um modelo de plugin para copilot',
+            part2: 'Edite <1>src/pages/Home.js</1> e salve para recarregar.'
+        }
+    },
+    link: {
+        blipDataExample: 'Obtenha dados do blip',
+        swrExemple: 'Buscar dados com SWR'
+    },
+    button: {
+        repository: 'Visite o Repositório'
+    },
+    menu: {
+        goBack: 'Voltar'
+    },
+    name: 'Nome',
+    shortName: 'Nome abreviado',
+    email: 'E-mail',
+    phone: 'Telefone',
+    identity: 'Identificador',
+    identifier: 'Identificador',
+    language: 'Idioma',
+    accessKey: 'Chave de acesso',
+    template: 'Template',
+    creationDate: 'Data de criação'
+};
+
+export default translations;
